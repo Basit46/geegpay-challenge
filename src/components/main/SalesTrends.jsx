76bc2448ef58f71e6f2ece96f8875ee45d7e6575
Basit46 <Hassanbasit1990@gmail.com>
2024-01-27 09:05:59 +0100

    Added changes

--- src/components/main/SalesTrends.jsx
+++ src/components/main/SalesTrends.jsx
@@ -18,16 +18,22 @@ const SalesTrends = () => {
           <p className="text-[14px] font-medium">Short by:</p>
           <span
             tabIndex={1}
-            className={`${
-              isDarkMode && "bg-white text-black"
-            } flex gap-[10px] items-center rounded-[20px] group border border-[#E1DFDF] py-[6px] px-[12px]`}
+            className={`flex gap-[10px] items-center rounded-[20px] group border border-[#E1DFDF] py-[6px] px-[12px]`}
           >
             <p className="text-[12px]">Weekly</p>
-            <img
-              className="w-[20px] h-[20px] group-focus:border group-focus:border-gray-700"
-              src={arrow}
-              alt="arrow"
-            />
+            <svg
+              tabIndex={1}
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+            >
+              <path
+                d="M3.19841 6.20675C3.43891 5.95614 3.81525 5.93336 4.08045 6.1384L4.15643 6.20675L10 12.2955L15.8436 6.20675C16.0841 5.95614 16.4604 5.93336 16.7256 6.1384L16.8016 6.20675C17.0421 6.45735 17.064 6.84951 16.8672 7.12585L16.8016 7.20502L10.479 13.7933C10.2385 14.0439 9.86217 14.0666 9.59697 13.8616L9.52099 13.7933L3.19841 7.20502C2.93386 6.92935 2.93386 6.48241 3.19841 6.20675Z"
+                fill={isDarkMode ? "white" : "#0D062D"}
+              />
+            </svg>
           </span>
         </div>
       </div>
